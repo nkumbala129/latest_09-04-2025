1d49beb03e1466259f6e192982bcd7344e3034e9
nkumbala129 <narenderkumbalak@gmail.com>
2025-04-09 20:25:37 +0530

    Update trial12.py

--- trial12.py
+++ trial12.py
@@ -23,7 +23,7 @@ SEMANTIC_MODEL = '@"CORTEX_SEARCH_TUTORIAL_DB"."PUBLIC"."MULTIFAMILYSTAGE"/Green
 # Streamlit Page Config
 st.set_page_config(
     page_title="Welcome to Cortex AI Assistant",
-    layout="wide",
+    layout="middle",
     initial_sidebar_state="auto"
 )
 
